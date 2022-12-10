51b5366c78d9428a5dc114a1dbbc5c8659f2e3e0
Stefan <dutchdelphidude@gmail.com>
2022-12-10 20:14:28 +0100

    Windows Mutex

--- Militereum.dpr
+++ Militereum.dpr
@@ -3,18 +3,41 @@ program Militereum;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  main in 'main.pas' {FrmMain},
-  server in 'server.pas',
-  log in 'log.pas' {FrmLog},
-  transaction in 'transaction.pas',
+{$IFDEF MSWINDOWS}
+  System.Classes,
+  WinAPI.Windows,
+{$ENDIF}
   approve in 'approve.pas' {FrmApprove},
   common in 'common.pas',
-  thread in 'thread.pas';
+  log in 'log.pas' {FrmLog},
+  main in 'main.pas' {FrmMain},
+  server in 'server.pas',
+  thread in 'thread.pas',
+  transaction in 'transaction.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
+{$IFDEF MSWINDOWS}
+  var window: HWND;
+  const MessageWindowClassName = 'MilitereumMessageWindow';
+  const mutex = CreateMutex(nil, False, 'MilitereumMutex');
+  if (mutex = 0) or (GetLastError = ERROR_ALREADY_EXISTS) then
+  begin
+    window := FindWindow(PChar(MessageWindowClassName), nil);
+    if window <> 0 then
+      SendMessage(window, common.CM_SHOW, 0, 0);
+    EXIT;
+  end;
+{$ENDIF}
   Application.CreateForm(TFrmMain, FrmMain);
+{$IFDEF MSWINDOWS}
+  window := common.allocateHwnd(MessageWindowClassName, FrmMain.MessageWindowProc);
+{$ENDIF}
   Application.Run;
+{$IFDEF MSWINDOWS}
+  if window <> 0 then DeallocateHwnd(window);
+  if mutex <> 0 then CloseHandle(mutex);
+{$ENDIF}
 end.
