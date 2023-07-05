290d1fbe226a58ad1b8df62694886483b952cbcf
Stefan <dutchdelphidude@gmail.com>
2023-07-05 14:14:07 +0200

    check for setApprovalForAll(address,bool)

--- main.pas
+++ main.pas
@@ -415,7 +415,7 @@ begin
               callback(allow);
             end;
 
-            next([Step1, Step2, Step3, Step4, Step5, Step6, Step7, Step8, Step9, Step10], 0, [],
+            next([Step1, Step2, Step3, Step4, Step5, Step6, Step7, Step8, Step9, Step10, Step11], 0, [],
               procedure // block
               begin
                 done(False);
