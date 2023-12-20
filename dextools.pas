5ea222fc101d91c1932cf1a1f28e3c4ad811b80e
Stefan <dutchdelphidude@gmail.com>
2023-12-20 16:43:41 +0100

    DEXTools v1 -> v2

--- dextools.pas
+++ dextools.pas
@@ -6,105 +6,115 @@ uses
   // Delphi
   System.JSON,
   System.SysUtils,
-  System.Types,
   // web3
   web3;
 
-function token(const apiKey: string; const chain: TChain; const address: TAddress; const callback: TProc<TJsonValue, IError>): IAsyncResult;
-function pairs(const apiKey: string; const chain: TChain; const address: TAddress; const callback: TProc<TJsonArray, IError>): IAsyncResult;
-function score(const apiKey: string; const chain: TChain; const address: TAddress; const callback: TProc<Integer, IError>): IAsyncResult;
+procedure pairs(const apiKey: string; const chain: TChain; const address: TAddress; const callback: TProc<TJsonArray, IError>);
+procedure score(const apiKey: string; const chain: TChain; const address: TAddress; const callback: TProc<Integer, IError>);
 
 implementation
 
 uses
   // Delphi
-  System.Generics.Collections,
-  System.Math,
+  System.DateUtils,
   System.Net.URLClient,
   // web3
-  web3.eth.types,
   web3.http,
   web3.json;
 
-function network(chain: TChain): string; inline;
+const DEXTOOLS_API_BASE = 'https://open-api.dextools.io/free/';
+
+function network(const chain: TChain): IResult<string>;
 begin
-  if chain = Ethereum then
-    Result := 'ether'
-  else if chain = Arbitrum then
-    Result := 'arbitrum'
+  if chain = Arbitrum then
+    Result := TResult<string>.Ok('arbitrum')
   else if chain = BNB then
-    Result := 'bsc'
+    Result := TResult<string>.Ok('bsc')
   else if chain = Base then
-    Result := 'base'
+    Result := TResult<string>.Ok('base')
+  else if chain = Ethereum then
+    Result := TResult<string>.Ok('ether')
+  else if chain = Goerli then
+    Result := TResult<string>.Ok('ethergoerli')
+  else if chain = Fantom then
+    Result := TResult<string>.Ok('fantom')
+  else if chain = Gnosis then
+    Result := TResult<string>.Ok('gnosis')
+  else if chain = Optimism then
+    Result := TResult<string>.Ok('optimism')
+  else if chain = Polygon then
+    Result := TResult<string>.Ok('polygon')
+  else if chain = Pulsechain then
+    Result := TResult<string>.Ok('pulse')
   else
-    Result := '';
+    Result := TResult<string>.Err('', TError.Create('%s not supported', [chain.Name]));
 end;
 
-// get dextools' token information for given token address
-function token(const apiKey: string; const chain: TChain; const address: TAddress; const callback: TProc<TJsonValue, IError>): IAsyncResult;
+procedure pairs(const apiKey: string; const chain: TChain; const address: TAddress; const callback: TProc<TJsonArray, IError>);
 begin
-  Result := nil;
-  const network = network(chain);
-  if network = '' then
-    callback(nil, TError.Create('%s not supported', [chain.Name]))
-  else
-    Result := web3.http.get(
-      Format('https://api.dextools.io/v1/token?chain=%s&address=%s', [network, address.ToChecksum]), [
-        TNetHeader.Create('Accept', 'application/json'),
-        TNetHeader.Create('X-API-Key', apiKey)
-      ], callback
-    );
+  network(chain)
+    .ifErr(procedure(err: IError)
+    begin
+      callback(nil, err)
+    end)
+    .&else(procedure(network: string)
+    begin
+      web3.http.get(DEXTOOLS_API_BASE + Format('v2/token/%s/%s/pools?sort=creationTime&order=asc&from=2000-01-01&to=%4d-%2d-%2d', [network, address, YearOf(System.SysUtils.Now), MonthOf(System.SysUtils.Now), DayOf(System.SysUtils.Now)]), [TNetHeader.Create('X-BLOBR-KEY', apiKey)],
+        procedure(response: TJsonValue; err: IError)
+        begin
+          if Assigned(err) then
+          begin
+            callback(nil, err);
+            EXIT;
+          end;
+          const data = getPropAsObj(response, 'data');
+          if not Assigned(data) then
+          begin
+            callback(nil, TError.Create('data is null'));
+            EXIT;
+          end;
+          const results = getPropAsArr(data, 'results');
+          if not Assigned(results) then
+          begin
+            callback(nil, TError.Create('results is null'));
+            EXIT;
+          end;
+          callback(results, nil);
+        end);
+    end);
 end;
 
-// get DEX token pairs for given token address
-function pairs(const apiKey: string; const chain: TChain; const address: TAddress; const callback: TProc<TJsonArray, IError>): IAsyncResult;
+procedure score(const apiKey: string; const chain: TChain; const address: TAddress; const callback: TProc<Integer, IError>);
 begin
-  Result := token(apiKey, chain, address, procedure(obj: TJsonValue; err: IError)
-  begin
-    if Assigned(err) then
+  network(chain)
+    .ifErr(procedure(err: IError)
     begin
-      callback(nil, err);
-      EXIT;
-    end;
-    const data = getPropAsObj(obj, 'data');
-    if not Assigned(data) then
+      callback(0, err)
+    end)
+    .&else(procedure(network: string)
     begin
-      callback(nil, TError.Create('data is null'));
-      EXIT;
-    end;
-    const pairs = getPropAsArr(data, 'pairs');
-    if Assigned(pairs) then
-      callback(pairs, nil)
-    else
-      callback(nil, TError.Create('pairs is null'));
-  end);
-end;
-
-// returns the lowest %address%-WETH score where %address% is the token address
-function score(const apiKey: string; const chain: TChain; const address: TAddress; const callback: TProc<Integer, IError>): IAsyncResult;
-begin
-  Result := nil;
-  if chain.WETH = '' then
-    callback(0, TError.Create('%s not supported', [chain.Name]))
-  else
-    Result := pairs(apiKey, chain, address, procedure(pairs: TJsonArray; err: IError)
-    begin
-      if Assigned(err) then
-      begin
-        callback(0, err);
-        EXIT;
-      end;
-      var score := 0;
-      try
-        for var I := 0 to Pred(pairs.Count) do
-          if TAddress(getPropAsStr(getPropAsObj(pairs[I], 'tokenRef'), 'address')).SameAs(chain.WETH) then
-            if score = 0 then
-              score := getPropAsInt(pairs[I], 'dextScore')
-            else
-              score := Min(score, getPropAsInt(pairs[I], 'dextScore'));
-      finally
-        callback(score, nil);
-      end;
+      web3.http.get(DEXTOOLS_API_BASE + Format('v2/token/%s/%s/score', [network, address]), [TNetHeader.Create('X-BLOBR-KEY', apiKey)],
+        procedure(response: TJsonValue; err: IError)
+        begin
+          if Assigned(err) then
+          begin
+            callback(0, err);
+            EXIT;
+          end;
+          const data = getPropAsObj(response, 'data');
+          if not Assigned(data) then
+          begin
+            callback(0, TError.Create('data is null'));
+            EXIT;
+          end;
+          const score = getPropAsObj(data, 'dextScore');
+          if not Assigned(score) then
+          begin
+            callback(0, TError.Create('dextScore is null'));
+            EXIT;
+          end;
+          callback(getPropAsInt(score, 'total'), nil);
+        end);
     end);
 end;
 
